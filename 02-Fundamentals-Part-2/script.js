114d57e071e4dad7dca1be0634bac4db522178f8
Tsegay Hailemariam <tsegayhailemariam444@gmail.com>
2023-02-16 12:23:34 +0300

    Deleting the orginal files and creating and updating new file accordingly

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -107,7 +107,7 @@ const percentage = [percentageOfWorld1(population[0]),
 percentageOfWorld1(population[1]), percentageOfWorld1(population[2]),
 percentageOfWorld1(population[3])]
 console.log(percentage);
-*/
+
 const calcTip = function (bill) {
     if (bill >= 50 && bill <= 300) {
         return bill * 0.15;
@@ -123,3 +123,4 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 console.log(tips);
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(total);
+*/
