8b94875fcf91254e89a3443c61ddd61a20963659
Tsegay Hailemariam <tsegayhailemariam444@gmail.com>
2023-02-17 15:33:05 +0300

    Commiting the first stage of environment setting up

---
+++ 03-Developer-Skills/script.js
@@ -0,0 +1,9 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const tse = 'Tsegay HAilemariam';
+const t = 23;
+console.log(tse);
+console.log(t);
+console.log('Hello Tsegay this is live server from nodejs');
+console.log('Display this text without reloading the page explicitly');
