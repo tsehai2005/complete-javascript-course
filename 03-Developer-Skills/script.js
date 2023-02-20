63e4121bf9b303075e8594fbec641ec9ea216970
Tsegay Hailemariam <tsegayhailemariam444@gmail.com>
2023-02-20 20:53:39 +0300

    Commiting Learning How to Code

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,9 +1,30 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
-
+/*
 const tse = 'Tsegay HAilemariam';
 const t = 23;
 console.log(tse);
 console.log(t);
 console.log('Hello Tsegay this is live server from nodejs');
 console.log('Display this text without reloading the page explicitly');
+
+//A) Understand the problem
+// - what are the number of days in the string? Answer Indexof()+1;
+// - how to change arrrays into strings and concatnatethem with strings
+//B)Breaking up the problem into sub-problems
+// - change the array into strings
+// - change each elementinto string
+// -string need to contain day (indexof() + 1)
+const dataSet1 = [17, 21, 23];
+const dataSet2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}°C in ${i + 1} days ... `;
+  }
+  console.log('... ' + str);
+};
+printForecast(dataSet1);
+printForecast(dataSet2);
+*/
