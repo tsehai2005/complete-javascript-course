6c954c0a7625049771657fdd38c6e3dcab42ab89
Tsegay Hailemariam <tsegayhailemariam444@gmail.com>
2023-02-22 22:34:33 +0300

    Implementation of modal windows

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -3,9 +3,9 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
 
-const openModal = function () {
+const showModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -15,15 +15,13 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', showModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
-
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
