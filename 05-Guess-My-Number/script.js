7acb6e0c4a9ec3564bdc639f10b6a5140b0a8eee
Tsegay Hailemariam <tsegayhailemariam444@gmail.com>
2023-02-22 15:33:07 +0300

    refactoring the code

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -12,16 +12,21 @@ console.log(document.querySelector('.guess').value);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   // When there is no input or the input is  zero
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number';
+    //document.querySelector('.message').textContent = '⛔ No number';
+    displayMessage('⛔ No number');
   }
   // When the player guess correctly
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number !';
+    //document.querySelector('.message').textContent = '🎉 Correct Number !';
+    displayMessage('🎉 Correct Number !');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -29,34 +34,51 @@ document.querySelector('.check').addEventListener('click', function () {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-  } // When guess is too high
-  else if (guess > secretNumber) {
+  }
+  // if the guess and the secret number are not the same
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game';
+      //document.querySelector('.message').textContent = '💥 You lost the game';
+      displayMessage('💥 You lost the game');
       document.querySelector('.score').textContent = 0;
     }
   }
-  //When guess is too low
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game';
-      document.querySelector('.score').textContent = 0;
-    }
-  }
+
+  // // When guess is too high
+  // else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Too high!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
+  // //When guess is too low
+  // else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📉 Too low!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  //document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
   document.querySelector('.number').textContent = '?';
